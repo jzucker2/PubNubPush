f35f829a06727f8d5f5c0b458095784b05ddaac7
Jordan Zucker <jordan.zucker@gmail.com>
2016-09-27 13:00:10 -0700

    Saving with simple push samples

---
+++ ObjCPush/ObjCPush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCPush
+//
+//  Created by Jordan Zucker on 9/27/16.
+//  Copyright © 2016 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+workspace 'Push'
+project 'ObjCPush/ObjCPush'
+project 'SwiftPush/SwiftPush'
+
+target 'ObjCPush' do
+	pod 'PubNub'
+	project 'ObjCPush/ObjCPush'
+end
+
+
+target 'SwiftPush' do
+	pod 'PubNub'
+	project 'SwiftPush/SwiftPush'
+end
